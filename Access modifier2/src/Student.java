686fcc0130fe2858bbf0ae67e3c06cccf9c5489d
Nguyen Chon Loi <chonloi2561998@gamil.com>
2022-09-16 15:38:22 +0700

    done

---
+++ Access modifier2/src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private String name = "Jonh";
+    private String classes = "C02";
+
+    public Student() {
+        setClasses(name);
+        setClasses(classes);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ Access modifier2/src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+
+        s1.setName("Duy");
+
+        s1.setClasses("C0722H1");
+
+
+        s2.setName("Cuong");
+
+        s2.setClasses("C0722H1");
+
+    }
+}
